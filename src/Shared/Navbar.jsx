77453f1691f4a93448d5a0e492694fbcb68f8a3f
rifat3568 <mazharulislam3569@gmail.com>
2024-06-19 19:17:03 +0600

    navbar add

--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -1,33 +1,69 @@
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
         <div>
-            <div className="navbar bg-base-100">
+            <div className="navbar bg-base-100 font-roboto mt-2">
   <div className="navbar-start">
     <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+      <div tabIndex={0} role="button" className="lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
-       
-        <li><a>Item 3</a></li>
+      <li>
+      <Link>Home</Link>
+      </li>
+      <li>
+      <Link>Categories</Link>
+      </li>
+      <li>
+      <Link>About Us</Link>
+      </li>
+      <li>
+      <Link>Products</Link>
+      </li>
+      <li>
+      <Link>Pet Care</Link>
+      </li>
+      <li>
+      <Link>Contacts</Link>
+      </li>
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">Glocal</a>
+    <img src="https://i.ibb.co/PG457mG/Group-7.png" className="w-10" alt="" />
+
+    <p className="text-2xl font-bold text-[#FFE55E]"><span className="text-[#FFAA00]">G</span>local</p>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
+     
       <li>
-        
+      <Link>Home</Link>
       </li>
-      <li><a>Item 3</a></li>
+      <li>
+      <Link>Categories</Link>
+      </li>
+      <li>
+      <Link>About Us</Link>
+      </li>
+      <li>
+      <Link>Products</Link>
+      </li>
+      <li>
+      <Link>Pet Care</Link>
+      </li>
+      <li>
+      <Link>Contacts</Link>
+      </li>
+     
     </ul>
   </div>
-  <div className="navbar-end">
-    <a className="btn">Button</a>
+  <div className="navbar-end flex gap-4 font-semibold">
+    <a className="bg-[#124C5F] text-white p-2 lg:px-8 rounded-md">Sign In</a>
+    <a className="bg-[#C7E7E1] text-[#124C5F] p-2 lg:px-8 rounded-md">Register</a>
+
   </div>
+  
 </div>
         </div>
     );
